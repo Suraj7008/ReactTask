9f0fb66c58b3f3ba5d6f7168961e03bb3dd9b5dd
suraj pawar <Suraj@tekvision.in>
2023-07-20 14:02:53 +0530

    NEW PROJECT

--- helloreact/src/App.js
+++ helloreact/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  // const showText => () {
-  //   <Im==
-  // }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          Hello World
-          <br></br>
-          <button>
-            <a href='#taphere' /*onClick='showText'*/>Tap Here</a>
-          </button>
-        </div>
-      </header>
-    </div>
-  );
-}
+// function App() {
+
+//   return (
+//         <div>
+//           Hello World
+//           <br></br>
+//           <button>
+//             <a href='#taphere' /*onClick='showText'*/>Tap Here</a>
+//           </button>
+//         </div>
+//   );
+// }
+
+const App = () =>
+  <div>
+    <h1>Hello World</h1>
+    <br>
+    </br>
+    <button>
+      Tap Here
+    </button>
+  </div>
 
 export default App;
