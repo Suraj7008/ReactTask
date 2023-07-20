7e2072f78321034a0e7fb2aff03c56c30cdc9b84
suraj pawar <Suraj@tekvision.in>
2023-07-20 09:45:46 +0530

    reacts

--- helloreact/src/App.js
+++ helloreact/src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const showText => () {
+  //   <Im==
+  // }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           Hello World
+          <br></br>
+          <button>
+            <a href='#taphere' /*onClick='showText'*/>
+              Tap Here
+            </a>
+          </button>
         </div>
       </header>
     </div>
